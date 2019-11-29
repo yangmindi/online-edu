7b801ce06742a119dc81ded3d0677f112af1864a
ymd <1946334058@qq.com>
2019-11-29 23:32:00 +0800

    MP代码生成器

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/TeacherService.java
@@ -0,0 +1,16 @@
+package com.guli.edu.service;
+
+import com.guli.edu.entity.Teacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author Helen
+ * @since 2019-11-29
+ */
+public interface TeacherService extends IService<Teacher> {
+
+}
