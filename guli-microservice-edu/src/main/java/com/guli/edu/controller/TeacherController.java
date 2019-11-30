ae885c3b632d0e474aa0e4a6f2009978fb38a5d1
ymd <1946334058@qq.com>
2019-11-30 23:52:37 +0800

    简单分页查询功能

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/TeacherController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/TeacherController.java
@@ -1,6 +1,7 @@
 package com.guli.edu.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.guli.common.vo.R;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.service.TeacherService;
@@ -24,6 +25,18 @@ public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
+
+    //3.分页查询讲师列表的方法
+    @GetMapping("{page}/{limit}")
+    public R pageList(@PathVariable Long page,
+                      @PathVariable Long limit){
+        Page<Teacher> pageParm = new Page<>(page,limit);
+        teacherService.page(pageParm,null);
+        List<Teacher> records = pageParm.getRecords();
+        long total = pageParm.getTotal();
+
+        return R.ok().data("total",total).data("rows",records);
+    }
 
     //1.查询所有的讲师功能
     @ApiOperation(value = "所有讲师列表")
